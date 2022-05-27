385038d4d99507d65d125919b7c3624b160729b3
Abhishek Kumar <kumarabhishek063@outlook.com>
2022-05-27 20:01:21 +0530

    Added ACK page.

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
 import "./Home.css";
+import Popup from './Popup';
+
 
 function Home() {
   const [projectName, setProjectName] = useState("");
   const [frontend, setFrontend] = useState("");
   const [backend, setBackend] = useState("");
 
-  const flutterArr = ["nodejs", "laravel", "django"];
-  const reactArr = ["nodejs", "django", "firebase"];
+  const flutterArr = ["Nodejs", "Laravel", "Django"];
+  const reactArr = ["Nodejs", "Django", "Firebase"];
 
   function changeProject(event) {
     setProjectName(event.target.value);
@@ -21,24 +22,36 @@ function Home() {
   function changeBackend(event) {
     setBackend(event.target.value);
   }
+  const [isOpen, setIsOpen] = useState(false);
+ 
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+    console.log("Modal reached")
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     const url = `https://github.com/createadi/${frontend}-with-${backend}/archive/refs/heads/main.zip`;
 
     console.log(url);
+   
     window.location.replace(url);
+    togglePopup()
   }
+
+ 
+ 
+
 
   return (
     <div className="home">
       <div className="brand_container">
         <div className="brand_name">Flexy</div>
-        <div className="brand_description">Get started with choice.</div>
+        <div className="brand_description">Lets get STARTED...</div>
       </div>
 
       <div className="form_container">
-        <div className="form_des">Setup your preferred stack.</div>
+        <div className="form_des">Setup your preferred stack</div>
         <form className="formFields" action="" onSubmit={handleSubmit}>
           <input
             type="text"
@@ -50,7 +63,7 @@ function Home() {
           />
           <select name="frontend" id="" onChange={changeFrontend}>
             <option value="" disabled selected>
-              Select your Frontend
+              Select your Frontend 
             </option>
             <option value="flutter">Flutter</option>
             <option value="react">React</option>
@@ -65,10 +78,56 @@ function Home() {
               : reactArr.map((item) => <option value={item}>{item}</option>)}
           </select>
 
-          <button className="createButton" type="submit" onClick={handleSubmit}>
+          {/* <button className="createButton" type="submit" onClick={togglePopup}>
             Create Project
-          </button>
+          </button> */}
+          <div>
+            <input
+              type="button"
+              value="Create Project 🎁"
+              onClick={togglePopup}
+            />
+            {/* <p>Thank You!</p> */}
+            {isOpen && <Popup
+              content={<>
+                <h2>Thank You!</h2>
+                <br></br>
+                <p><h3>Please follow the steps given in Readme file.</h3>
+                
+                  <p>
+                  <img src="https://img.icons8.com/office/48/000000/react.png" alt='react'/> 
+                  <img src="https://img.icons8.com/color/48/000000/flutter.png" alt='flutter'/> 
+                  <img src="https://img.icons8.com/color/48/000000/nodejs.png" alt='nodeJs'/> 
+                  <img src="https://img.icons8.com/color/48/000000/django.png" alt='django'/>
+                  <img src="https://img.icons8.com/color/48/000000/google-firebase-console.png" alt="firebase"/>
+                  <img src="https://img.icons8.com/fluency/48/000000/laravel.png" alt='laravel'/> 
+                  </p>
+                </p>
+                <input
+              type="button"
+              value="Download Project 🎁"
+              onClick={handleSubmit}
+              
+            />
+              </>}
+              handleClose={togglePopup}
+            />}
+          </div>
+          
+
         </form>
+        <div>
+        <p>
+          <h2>Supported frameworks:</h2>           
+              <img style={{padding: '12px 12px'}} src="https://img.icons8.com/officel/80/000000/react.png" alt='react'/> 
+              <img style={{padding: '12px 12px'}}  src="https://img.icons8.com/color/80/000000/flutter.png" alt='flutter'/> 
+              <img style={{padding: '12px 12px'}}  src="https://img.icons8.com/color/80/000000/nodejs.png" alt='nodeJs'/> 
+              <img style={{padding: '12px 12px'}}  src="https://img.icons8.com/color/80/000000/django.png" alt='django'/>
+              <img style={{padding: '12px 12px'}}  src="https://img.icons8.com/color/80/000000/google-firebase-console.png" alt="firebase"/>
+              <img style={{padding: '12px 12px'}}  src="https://img.icons8.com/fluency/80/000000/laravel.png" alt='laravel'/> 
+            </p>
+          
+        </div>
       </div>
     </div>
   );
